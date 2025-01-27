6e9a8881b02f3b44fefb13114eae16f224ee2407
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2025-01-27 13:11:07 +0200

    update instagram link

--- src/components/socials_comp.tsx
+++ src/components/socials_comp.tsx
@@ -5,7 +5,7 @@ export const Socials=()=>{
                     <p className="p_text">Follow Us On Our Socials</p>
                 </div>
                 <div className="d-flex flex-row justify-content-center">
-                    <a href="https://www.instagram.com/purplepagesglobal" target="_blank"><i className="bi bi-instagram me-3 p_text"></i></a>
+                    <a href="https://www.instagram.com/purplepagesint" target="_blank"><i className="bi bi-instagram me-3 p_text"></i></a>
                     <a href="https://www.linkedin.com/company/purple-pages-global" target="_blank"><i className="bi bi-linkedin me-3 p_text"></i></a>
                     <a href="https://www.facebook.com/purplepagesglobal" target="_blank"><i className="bi bi-facebook me-3 p_text"></i></a>
                     <a href="mailto:info@purplepageszim.com" target="_blank"><i className="bi bi-envelope-at  p_text"></i></a>
